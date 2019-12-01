8a1aaa91a39f11e0dc16888bafde67ddd63a6a82
Joshvu93 <joshvu93@gmail.com>
2019-11-30 22:09:39 -0800

    Added actions to each components

--- src/js/components/ExpenseEntries/expenseReducer.js
+++ src/js/components/ExpenseEntries/expenseReducer.js
@@ -5,5 +5,27 @@ const defaultState = {
 };
 
 export default function ExpenseReducer(state = defaultState, action) {
-  return state;
+  const { type, payload } = action;
+  switch (type) {
+    case 'UPDATE_EXPENSE_DESCRIPTION': {
+      return {
+        ...state,
+        description: payload.description
+      };
+    }
+    case 'ADD_EXPENSE': {
+      const { description, amount } = action.payload;
+      return {
+        description: '',
+        action: '',
+        lineItems: [
+          ...state.lineItems,
+          { description, amount }
+        ]
+      };
+    }
+    default: {
+      return state;
+    }
+  }
 }
--- src/js/components/IncomeEntries/incomeReducer.js
+++ src/js/components/IncomeEntries/incomeReducer.js
@@ -4,6 +4,28 @@ const defaultState = {
   lineItems: []
 };
 
-export default function IncomeReducer (state = defaultState, action){
-  return state;
+export default function IncomeReducer(state = defaultState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case 'UPDATE_INCOME_DESCRIPTION': {
+      return {
+        ...state,
+        description: payload.description
+      };
+    }
+    case 'ADD_INCOME': {
+      const { description, amount } = action.payload;
+      return {
+        description: '',
+        action: '',
+        lineItems: [
+          ...state.lineItems,
+          { description, amount }
+        ]
+      };
+    }
+    default: {
+      return state;
+    }
+  }
 }
